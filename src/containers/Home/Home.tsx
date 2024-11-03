b80d2cae40abf8c3bf855d8700c30914aa8cac73
Akylbek <hsplay74@gmail.com>
2024-11-03 21:43:32 +0600

    v1

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,8 +1,49 @@
+import axiosApi from "../../axiosApi.ts";
+import { useCallback, useEffect, useState } from "react";
+import {Page} from "../../types.ts";
+import { useParams } from "react-router-dom";
+import Loader from "../../components/Loader/Loader.tsx";
 
 const Home = () => {
-    return (
-        <div>
+    const { pageName } = useParams();
+    const [spinner, setSpinner] = useState(false);
+    const [page, setPage] = useState<Page>({
+        name: '',
+        title: '',
+        content: '',
+    });
 
+    const getData = useCallback(async () => {
+        try {
+            const { data } = await axiosApi.get<Page | null>(`pages/${pageName}.json`);
+            if (data === null) {
+                setPage({
+                    title: '',
+                    name: '',
+                    content: ''
+                })
+            } else {
+                setSpinner(true);
+                setPage(data);
+                setSpinner(false);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, [pageName]);
+
+    useEffect(() => {
+        getData();
+    }, [getData, pageName]);
+
+    if (spinner) {
+        return <Loader />
+    }
+
+    return (
+        <div className="text-center mt-5 border b-1 p-3 shadow">
+            <h1 className="h1">{page.title}</h1>
+            <p className="fs-5">{page.content}</p>
         </div>
     );
 };
